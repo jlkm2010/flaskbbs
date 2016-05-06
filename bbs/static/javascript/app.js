b0f77487621d483856df53e778c76cd3c7f1fb79
hc <1440557925@qq.com>
2016-05-06 22:35:24 +0800

    delete unused file

--- bbs/static/javascript/app.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Administrator on 2015/8/3.
- */
-$(function(){
-    $('.nav li a').click(function(){
-        $('.nav li').removeClass('active');
-        $(this).parent().addClass('active');
-    })
-});
